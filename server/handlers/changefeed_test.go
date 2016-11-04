d4896db35214968f616d79e3914eccfb6495e961
David Lawrence <david.lawrence@docker.com>
2016-11-04 11:38:30 -0700

    tests for changefeed handler Signed-off-by: David Lawrence <david.lawrence@docker.com> (github: endophage)

---
+++ server/handlers/changefeed_test.go
@@ -0,0 +1,198 @@
+package handlers
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	ctxu "github.com/docker/distribution/context"
+	"github.com/docker/notary"
+	"github.com/docker/notary/server/storage"
+	"github.com/stretchr/testify/require"
+)
+
+type changefeedArgs struct {
+	logger    ctxu.Logger
+	store     storage.MetaStore
+	imageName string
+	changeID  string
+	pageSize  uint64
+	reversed  bool
+}
+
+type changefeedTest struct {
+	name    string
+	args    changefeedArgs
+	want    []byte
+	wantErr bool
+}
+
+func Test_changefeed(t *testing.T) {
+	s := storage.NewMemStorage()
+
+	tests := []changefeedTest{
+		{
+			name: "Empty Store",
+			args: changefeedArgs{
+				logger:    logrus.New(),
+				store:     s,
+				imageName: "",
+				changeID:  "0",
+				pageSize:  notary.DefaultPageSize,
+				reversed:  false,
+			},
+			want:    []byte("{\"count\":0,\"records\":null}"),
+			wantErr: false,
+		},
+		{
+			name: "Bad ChangeID",
+			args: changefeedArgs{
+				logger:    logrus.New(),
+				store:     s,
+				imageName: "",
+				changeID:  "not_a_number",
+				pageSize:  notary.DefaultPageSize,
+				reversed:  false,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	runChangefeedTests(t, tests)
+
+}
+
+func runChangefeedTests(t *testing.T, tests []changefeedTest) {
+	for _, tt := range tests {
+		got, err := changefeed(tt.args.logger, tt.args.store, tt.args.imageName, tt.args.changeID, tt.args.pageSize, tt.args.reversed)
+		if tt.wantErr {
+			require.Error(t, err,
+				"%q. changefeed() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+		}
+		require.True(t, reflect.DeepEqual(got, tt.want),
+			"%q. changefeed() = %v, want %v", tt.name, string(got), string(tt.want))
+	}
+}
+
+func Test_checkChangefeedInputs(t *testing.T) {
+	type args struct {
+		logger ctxu.Logger
+		s      interface{}
+		ps     string
+		rev    string
+	}
+	s := storage.NewMemStorage()
+	tests := []struct {
+		name         string
+		args         args
+		wantStore    storage.MetaStore
+		wantPageSize uint64
+		wantReversed bool
+		wantErr      bool
+	}{
+		// Error cases
+		{
+			name: "No MetaStore",
+			args: args{
+				logger: logrus.New(),
+				s:      nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Bad page size",
+			args: args{
+				logger: logrus.New(),
+				s:      s,
+				ps:     "not_a_number",
+			},
+			wantErr:   true,
+			wantStore: s,
+		},
+		{
+			name: "Zero page size",
+			args: args{
+				logger: logrus.New(),
+				s:      s,
+				ps:     "0",
+			},
+			wantStore:    s,
+			wantPageSize: notary.DefaultPageSize,
+			wantReversed: false,
+		},
+		{
+			name: "Non-zero Page Size",
+			args: args{
+				logger: logrus.New(),
+				s:      s,
+				ps:     "10",
+			},
+			wantStore:    s,
+			wantPageSize: 10,
+			wantReversed: false,
+		},
+		{
+			name: "Reversed \"1\"",
+			args: args{
+				logger: logrus.New(),
+				s:      s,
+				ps:     "10",
+				rev:    "1",
+			},
+			wantStore:    s,
+			wantPageSize: 10,
+			wantReversed: true,
+		},
+		{
+			name: "Reversed \"true\"",
+			args: args{
+				logger: logrus.New(),
+				s:      s,
+				ps:     "10",
+				rev:    "true",
+			},
+			wantStore:    s,
+			wantPageSize: 10,
+			wantReversed: true,
+		},
+		{
+			name: "Reversed \"True\"",
+			args: args{
+				logger: logrus.New(),
+				s:      s,
+				ps:     "10",
+				rev:    "True",
+			},
+			wantStore:    s,
+			wantPageSize: 10,
+			wantReversed: true,
+		},
+		{
+			name: "Reversed \"false\"",
+			args: args{
+				logger: logrus.New(),
+				s:      s,
+				ps:     "10",
+				rev:    "false",
+			},
+			wantStore:    s,
+			wantPageSize: 10,
+			wantReversed: false,
+		},
+	}
+	for _, tt := range tests {
+		gotStore, gotPageSize, gotReversed, err := checkChangefeedInputs(tt.args.logger, tt.args.s, tt.args.ps, tt.args.rev)
+		if tt.wantErr {
+			require.Error(t, err,
+				"%q. checkChangefeedInputs() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+		}
+		require.True(t, reflect.DeepEqual(gotStore, tt.wantStore),
+			"%q. checkChangefeedInputs() gotStore = %v, want %v", tt.name, gotStore, tt.wantStore)
+
+		require.Equal(t, tt.wantPageSize, gotPageSize,
+			"%q. checkChangefeedInputs() gotPageSize = %v, want %v", tt.name, gotPageSize, tt.wantPageSize)
+
+		require.Equal(t, tt.wantReversed, gotReversed,
+			"%q. checkChangefeedInputs() gotReversed = %v, want %v", tt.name, gotReversed, tt.wantReversed)
+	}
+}
