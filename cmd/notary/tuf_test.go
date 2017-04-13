2a571363ed78b2bf51106d116f5d4ce1315b6d3d
Nassim 'Nass' Eddequiouaq <eddequiouaq.nassim@gmail.com>
2017-04-12 20:28:27 -0700

    Add test for import/export
    Signed-off-by: Nassim 'Nass' Eddequiouaq <eddequiouaq.nassim@gmail.com>

--- cmd/notary/tuf_test.go
+++ cmd/notary/tuf_test.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"io/ioutil"
 	"testing"
 
 	"github.com/docker/distribution/registry/client/auth"
@@ -13,7 +14,20 @@ import (
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 
+	"golang.org/x/net/context"
+	ctxu "github.com/docker/distribution/context"
+	"github.com/docker/notary/server/storage"
+	"github.com/docker/notary/server"
+	"github.com/Sirupsen/logrus"
+	"github.com/docker/notary/cryptoservice"
+	"github.com/docker/notary"
+	"github.com/docker/notary/trustmanager"
+
+
+
 	"github.com/docker/notary/tuf/data"
+	"github.com/docker/notary/passphrase"
+	"bytes"
 )
 
 func TestTokenAuth(t *testing.T) {
@@ -231,4 +245,107 @@ func TestPasswordStore(t *testing.T) {
 		ps.SetRefreshToken(myurl, "someService", "token") // doesn't return an error, just want to make sure no state changes
 		require.Equal(t, "", ps.RefreshToken(myurl, "someService"))
 	}
+}
+
+func TestExportImportAddFile(t *testing.T) {
+	setUp(t)
+
+	s := fullTestServer(t)
+	defer s.Close()
+
+	tempBaseDir := tempDirWithConfig(t, "{}")
+	defer os.RemoveAll(tempBaseDir)
+
+	tc := &tufCommander{
+		configGetter: func() (*viper.Viper, error) {
+			v := viper.New()
+			v.SetDefault("trust_dir", tempBaseDir)
+			v.SetDefault("remote_server.url", s.URL)
+			return v, nil
+		},
+
+		retriever: passphrase.ConstantRetriever("pass"),
+	}
+
+	url, err := tc.configGetter()
+	require.Equal(t, url.GetString("remote_server.url"), s.URL)
+
+	require.NoError(t, tc.tufInit(&cobra.Command{}, []string{"gun"}))
+
+	tc.sha256 = "88b76b34ab83a9e4d5abe3697950fb73f940aab1aa5b534f80cf9de9708942be"
+	require.NoError(t, tc.tufAddByHash(&cobra.Command{}, []string{"gun", "target1", "100"}))
+	require.NoError(t, tc.tufPublish(&cobra.Command{}, []string{"gun"}))
+
+	var buf1 bytes.Buffer
+	cmd := &cobra.Command{}
+	cmd.SetOutput(&buf1)
+	require.NoError(t, tc.tufList(cmd, []string{"gun"}))
+	require.Contains(t, buf1.String(), "target1")
+
+
+	tempExportDir, err := ioutil.TempDir("", "export-repo")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempExportDir)
+
+	tc = &tufCommander{
+		configGetter: func() (*viper.Viper, error) {
+			v := viper.New()
+			v.SetDefault("trust_dir", tempBaseDir)
+			v.SetDefault("remote_server.url", s.URL)
+			return v, nil
+		},
+		output: tempExportDir,
+
+		retriever: passphrase.ConstantRetriever("pass"),
+	}
+	require.NoError(t, tc.tufExportGUN(&cobra.Command{}, []string{"gun", "targets"}))
+	targetsJSONpath := filepath.Join(tempExportDir, "targets.json")
+	_, err = os.Stat(targetsJSONpath)
+	require.NoError(t, err)
+
+
+
+	targetFilePath := filepath.Join(tempBaseDir,"file2")
+	require.NoError(t, ioutil.WriteFile(targetFilePath, []byte("foo"), 0666))
+
+	tc = &tufCommander{
+		configGetter: func() (*viper.Viper, error) {
+			v := viper.New()
+			v.SetDefault("trust_dir", tempBaseDir)
+			v.SetDefault("remote_server.url", s.URL)
+			return v, nil
+		},
+		input: targetsJSONpath,
+		output: targetsJSONpath,
+
+		retriever: passphrase.ConstantRetriever("pass"),
+	}
+	require.NoError(t, tc.tufAdd(&cobra.Command{}, []string{"gun", "target2", targetFilePath}))
+	require.NoError(t, tc.tufImportGUN(&cobra.Command{}, []string{"gun", "targets", targetsJSONpath}))
+
+	var buf2 bytes.Buffer
+	cmd.SetOutput(&buf2)
+	require.NoError(t, tc.tufList(cmd, []string{"gun"}))
+	require.Contains(t, buf2.String(), "target1")
+	require.Contains(t, buf2.String(), "target2")
+}
+
+func fullTestServer(t *testing.T) *httptest.Server {
+	// Set up server
+	ctx := context.WithValue(
+		context.Background(), notary.CtxKeyMetaStore, storage.NewMemStorage())
+
+	// Do not pass one of the const KeyAlgorithms here as the value! Passing a
+	// string is in itself good test that we are handling it correctly as we
+	// will be receiving a string from the configuration.
+	ctx = context.WithValue(ctx, notary.CtxKeyKeyAlgo, "ecdsa")
+
+	// Eat the logs instead of spewing them out
+	var b bytes.Buffer
+	l := logrus.New()
+	l.Out = &b
+	ctx = ctxu.WithLogger(ctx, logrus.NewEntry(l))
+
+	cryptoService := cryptoservice.NewCryptoService(trustmanager.NewKeyMemoryStore(passphrase.ConstantRetriever("pass")))
+	return httptest.NewServer(server.RootHandler(ctx, nil, cryptoService, nil, nil, nil))
 }
