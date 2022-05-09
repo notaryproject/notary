dbfc28b258c8ae2212786861266db1f78e8bbd79
Jonny Stoten <jonny.stoten@docker.com>
2022-05-09 15:07:53 +0100

    Fix validate-vendor check
    Git now requires `safe.directory` config to be set if the repo has a
    different owner than the current user, eg. if the repo is bind-mounted
    into a container that is running as root.
    
    Signed-off-by: Jonny Stoten <jonny.stoten@docker.com>

--- buildscripts/circle-validate-vendor.sh
+++ buildscripts/circle-validate-vendor.sh
@@ -4,4 +4,4 @@ go_version=1.16.9
 
 docker run --rm --env GO111MODULE=on -w /notary --volume ${PWD}:/notary \
     golang:${go_version}-alpine \
-    sh -c "apk update && apk add bash git && buildscripts/validate-vendor.sh"
+    sh -c "apk update && apk add bash git && git config --global safe.directory /notary && buildscripts/validate-vendor.sh"
